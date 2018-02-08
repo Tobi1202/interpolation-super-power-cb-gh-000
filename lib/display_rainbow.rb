9646cd48d34b41e27591d58f31ffce7574b52182
TobiasBrodersen Brodersen <thetobiasbrodersen@gmail.com>
2018-02-08 11:58:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,11 +2,11 @@
 
 def display_rainbow(colors)
     puts "#{colors[0][0].uppercase}: #{colors[0]}";
-    
-      puts "#{colors[0][0].uppercase}: #{colors[0]}";
-        puts "#{colors[0][0].uppercase}: #{colors[0]}"
-          puts "#{colors[0][0].uppercase}: #{colors[0]}"
-            puts "#{colors[0][0].uppercase}: #{colors[0]}"
-              puts "#{colors[0][0].uppercase}: #{colors[0]}"
+      puts "#{colors[1][0].uppercase}: #{colors[1]}";
+        puts "#{colors[2][0].uppercase}: #{colors[2]}"
+          puts "#{colors[3][0].uppercase}: #{colors[3]}"
+            puts "#{colors[4][0].uppercase}: #{colors[4]}"
+              puts "#{colors[5][0].uppercase}: #{colors[5]}"
+                    puts "#{colors[6][0].uppercase}: #{colors[6]}"
 
 end
