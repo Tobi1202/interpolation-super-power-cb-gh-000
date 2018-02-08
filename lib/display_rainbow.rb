d3e9b8cb49ebfd423c4158a45023db29973ea534
TobiasBrodersen Brodersen <thetobiasbrodersen@gmail.com>
2018-02-08 11:47:56 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,10 @@
 # Write your #display_rainbow method here
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
-def display_rainbow(i = niel)
-  if(i != niel){ return colors[i]}
+def display_rainbow(i = 999)
+  if(i != 999){ return colors[i]}
 
     for(i=0; i< colors.size; i++ ){
       puts colors[i]
-
     }
 end
