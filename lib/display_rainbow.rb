545cba485448c2284efab505ffe90d3c0e57cbf5
TobiasBrodersen Brodersen <thetobiasbrodersen@gmail.com>
2018-02-08 11:57:26 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,6 @@
 # Write your #display_rainbow method here
 
 def display_rainbow(colors)
-
-    puts "#{colors[1][0].uppercase}: #{colors[i]}"
+    puts "#{colors[0][0].uppercase}: #{colors[0]}"
 
 end
